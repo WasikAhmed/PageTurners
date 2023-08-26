48bffb2ad6477969aa65aef3be8acf4094f72215
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2023-08-26 21:27:55 +0600

    added CategoryController and updated migration

--- PageTurnersWeb/Data/ApplicationDbContext.cs
+++ PageTurnersWeb/Data/ApplicationDbContext.cs
@@ -11,4 +11,13 @@ public class ApplicationDbContext : DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category {Id = 1, Name = "Action", DisplayOrder = 1},
+            new Category {Id = 2, Name = "History", DisplayOrder = 2},
+            new Category {Id = 3, Name = "SciFi", DisplayOrder = 3}
+            );
+    }
 }
