ce12dc18fe53b6b6ff963e4efad9df3ae521bbc9
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2023-08-26 22:45:22 +0600

    populated category list table from db

--- PageTurnersWeb/Controllers/CategoryController.cs
+++ PageTurnersWeb/Controllers/CategoryController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PageTurnersWeb.Data;
+using PageTurnersWeb.Models;
 
 namespace PageTurnersWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
