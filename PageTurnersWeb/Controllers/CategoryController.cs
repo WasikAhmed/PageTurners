bd315cef057a137191886deb2077df25e2b0c3ea
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2023-08-31 08:21:08 +0600

    updated CategoryController

--- PageTurnersWeb/Controllers/CategoryController.cs
+++ PageTurnersWeb/Controllers/CategoryController.cs
@@ -12,15 +12,17 @@ namespace PageTurnersWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ICategoryRepository _categoryRepo;
+        //private readonly ICategoryRepository _categoryRepo;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CategoryController(ICategoryRepository db)
+        public CategoryController(IUnitOfWork unitOfWork)
         {
-            _categoryRepo = db;
+            _unitOfWork = unitOfWork;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
+            //List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
             return View(objCategoryList);
         }
         public IActionResult Create()
@@ -41,8 +43,11 @@ namespace PageTurnersWeb.Controllers
             // }
             if (ModelState.IsValid)
             {
-                _categoryRepo.Add(obj);
-                _categoryRepo.save();
+                //_categoryRepo.Add(obj);
+                //_categoryRepo.save();
+
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
@@ -56,15 +61,17 @@ namespace PageTurnersWeb.Controllers
                 return NotFound();
             }
 
-            Category categoryFormDb = _categoryRepo.Get(u => u.Id == id);
+            //Category categoryFrommDb = _categoryRepo.Get(u => u.Id == id);
             // Category? categoryFromDb_1 = _db.Categories.FirstOrDefault(u => u.Id == id);
             // Category? categoryFromDb_2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-            if (categoryFormDb == null)
+
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+            if (categoryFromDb == null)
             {
                 return NotFound();
             }
 
-            return View(categoryFormDb);
+            return View(categoryFromDb);
         }
 
         [HttpPost]
@@ -72,8 +79,10 @@ namespace PageTurnersWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-                _categoryRepo.update(obj);
-                _categoryRepo.save();
+                //_categoryRepo.update(obj);
+                //_categoryRepo.save();
+                _unitOfWork.Category.update(obj);
+                _unitOfWork.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
@@ -88,26 +97,30 @@ namespace PageTurnersWeb.Controllers
                 return NotFound();
             }
 
-            Category? categoryFormDb = _categoryRepo.Get(u => u.Id == id);
-            if (categoryFormDb == null)
+            //Category? categoryFormDb = _categoryRepo.Get(u => u.Id == id);
+            Category? categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+            if (categoryFromDb == null)
             {
                 return NotFound();
             }
 
-            return View(categoryFormDb);
+            return View(categoryFromDb);
         }
 
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            Category? obj = _categoryRepo.Get(u => u.Id == id);
+            //Category? obj = _categoryRepo.Get(u => u.Id == id);
+            Category? obj = _unitOfWork.Category.Get(u => u.Id == id);
             if (obj == null)
             {
                 return NotFound();
             }
 
-            _categoryRepo.Remove(obj);
-            _categoryRepo.save();
+            //_categoryRepo.Remove(obj);
+            //_categoryRepo.save();
+            _unitOfWork.Category.Remove(obj);
+            _unitOfWork.Save();
             TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
