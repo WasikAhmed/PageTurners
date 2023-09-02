37c6abae5def6fd6b45c93ced761049213dbfefd
Wasik Ahmed Apon <apon.wasik2001@gmail.com>
2023-09-02 17:11:45 +0600

    add editor

--- PageTurners.Models/Product.cs
+++ PageTurners.Models/Product.cs
@@ -31,6 +31,7 @@ public class Product
     [Range(1,1000)]
     public double Price100 { get; set; }
 
+    [Display(Name = "Category")]
     public int CategoryId { get; set; }
     [ForeignKey("CategoryId")]
     [ValidateNever]
